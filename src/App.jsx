6cade07e183dbf19365e3597729d64e8fb00d2a2
arijitroy667 <arijitroy0445@gmail.com>
2025-03-16 11:13:16 +0530

    redux updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import authService from './appwrite/auth'
 import {login,logout} from './store/authSlice'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
@@ -27,7 +28,7 @@ function App() {
       <div className='w-full block'>
         <Header />
         <main>
-          {/* <Outlet /> */}
+          <Outlet />
         </main>
         <Footer />
       </div>
